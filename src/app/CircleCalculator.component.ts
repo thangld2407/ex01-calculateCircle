8ccac9cd13bf3ba8531dad36a585de56042e6c42
Thang Le Dinh <thangld2407@gmail.com>
2021-10-21 01:21:28 +0700

    add function auto calculate

--- src/app/CircleCalculator.component.ts
+++ src/app/CircleCalculator.component.ts
@@ -11,6 +11,10 @@ export class CircleCalculatorComponent {
   area:any = 0;
   isShow:boolean = false;
 
+  auto_perimter: any = 0;
+  auto_area: any = 0;
+
+
   handleCal() :void {
     this.isShow = true;
 
@@ -24,5 +28,10 @@ export class CircleCalculatorComponent {
 
   handleCalArea(radius: number) :any {
     return (Math.pow(radius, 2) * Math.PI).toFixed(2);
+  };
+
+  handleAutoCal() :void {
+    this.auto_perimter = this.handleCalPerimeter(this.radius);
+    this.auto_area = this.handleCalArea(this.radius);
   };
 };
